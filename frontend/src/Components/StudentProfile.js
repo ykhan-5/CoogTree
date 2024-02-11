a5e9a39faf35c1414eca183de4f9f8e2eff2eadb
linhcs <112646283+linhcs@users.noreply.github.com>
2024-02-11 16:51:28 +0000

    nothing

---
+++ frontend/src/Components/StudentProfile.js
@@ -0,0 +1,21 @@
+function StudentProfile() {
+    return (
+        <div>
+            
+            <div>
+                <img src = "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=" class="profile-picture"> </img>
+                <h1 className="name">First Name, Last Name</h1>
+                <p className= "current-classes">Currently enrolled:</p>
+
+                <p className= "social-media">Email</p>
+                <p className= "social-media">LinkedIn</p>
+                <p className= "social-media">Instagram</p>
+                <p className= "social-media">GitHub</p>
+
+                <a href="#"> </a>
+            </div>
+        </div>
+    )
+}
+
+export default StudentProfile;
