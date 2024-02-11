cd04607fc3eb3078897775cbd8635c42369aa9e8
Ayub Farah <ayubaf04@gmail.com>
2024-02-10 20:20:11 -0600

    Frontend Delete

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,17 @@ from flask import Flask, request, Response
 from dotenv import load_dotenv
 import os 
 from pymongo import MongoClient
-import json
+from operator import itemgetter
+
 
 load_dotenv()
 app = Flask(__name__)
-print(os.getenv("MONGO_URI"))
 client = MongoClient(os.getenv("MONGO_URI"))
 coogstreedb = client.get_database("coogsdb")
 
-# Get Routes
+optional_forms = lambda field, data : data[field] if field in data else ""  
+
+# Set Routes
 
 @app.route("/signup", methods=["POST"])
 def signup():
@@ -24,7 +26,13 @@ def signup():
     coogstreedb.get_collection("users").insert_one({
         "full_name": full_name,
         "email": email, 
-        "password": password
+        "password": password,
+        "majors": [],
+        "terms": set(), 
+        "classes": {},
+        "instagram": "",
+        "email": "",
+        "discord": ""
     })
     return Response("Created New User", status=200)
 
@@ -33,13 +41,36 @@ def login():
     email = request.json["email"]
     password = request.json["password"]
     user = coogstreedb.get_collection("users").find_one({
-        "email": email
+        "email": email,
     })
     if user and user["password"] == password: 
             return Response("Successfully Logged In", status=400) 
     return Response("Incorrect username or password", status=400)  
 
-# Set Routes
+@app.route("/setinfo", methods=["POST"])
+def set_info():
+    email, majors, terms, classes = itemgetter("instagram", "discord", "majors", "terms", "classes")(request.json)
+    if not coogstreedb.get_collection("users").find_one({
+         "email": email
+    }): return Response("User Not Found", status=400)
+    coogstreedb.get_collection("users").update_one({
+        "email": email
+    }, { "$set": {
+         "majors": majors, 
+         "terms": terms, 
+         "classes": classes,
+        "instagram": optional_forms("instagram", request.json),
+        "discord": optional_forms("discord", request.json)
+     }})  
+    return Response("User Info Updated", status=200)
+
+
+# Get Routes
+
+@app.route("/getclassmates", methods=["GET"])
+def get_class_mates():
+        class_code, prof, term = itemgetter("class_code", "prof", "term")(request.json)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
