b9c533b63cff6c1d13b9786d3b52978ba64512f9
linhcs <112646283+linhcs@users.noreply.github.com>
2024-02-11 18:23:10 +0000

    home

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,7 +1,8 @@
 function Home() {
     return (
         <div className="home">
-            <h1>Welcome To CoogTree</h1>
+            <h1 className="Header">Welcome To CoogTree</h1>
+            <img src= "https://cdn-icons-png.flaticon.com/512/2582/2582515.png" />
         </div>
     )
 }
