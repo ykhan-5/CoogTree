6d2d2569b5221a6aa10631a91f52d4b591fd7e46
Yusuf Khan <ykhan5@cougarnet.uh.edu>
2024-02-11 07:48:49 -0600

    Trying to get map to work.
    Commented out chart for now, trying to get it to work. Might be something with the file path.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import LoginPage from './Components/LoginPage';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import LoginPage from "./Components/LoginPage";
+// import Chart from "./Components/Chart";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <LoginPage/>
+      <Navbar />
+      <LoginPage />
+      {/* <Chart /> */}
     </div>
   );
 }
---
+++ frontend/src/Components/Chart.js
@@ -0,0 +1,23 @@
+// import React, { useEffect } from "react";
+// import define from "MapData/13ff3c0e37435870/runtime.js";
+// import { Runtime, Inspector } from "./MapData/13ff3c0e37435870/runtime.js";
+
+// const Chart = () => {
+//   useEffect(() => {
+//     const runtime = new Runtime();
+//     const main = runtime.module(define, Inspector.into(document.body));
+
+//     // Clean up function to remove the module when the component unmounts
+//     return () => {
+//       main.dispose();
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//     </div>
+//   );
+// };
+
+// export default Chart;
