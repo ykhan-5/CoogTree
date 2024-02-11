12ebeb020c463c6f3a6044620fa4f75246b2c085
Ayub Farah <ayubaf04@gmail.com>
2024-02-11 11:03:29 -0600

    Signout Button

--- frontend/src/Components/CoogNav.js
+++ frontend/src/Components/CoogNav.js
@@ -1,53 +1,37 @@
-import React from "react";
-import { Navbar, Nav, Col, Row, Button } from "react-bootstrap";
-import Cookies from "universal-cookie";
+import React from 'react'
+import {Navbar, Nav, Col, Row, Button} from 'react-bootstrap';
+import Cookies from 'universal-cookie';
+
 
 const CoogNav = () => {
   const cookie = new Cookies();
+  const handleSignout = () => {
+    cookie.remove('authed');
+    window.location.reload();
+  }
   return (
-    <div className="navb">
+    <div className="navbr">
       <Navbar>
-        <Navbar.Brand href="/">CoogTree</Navbar.Brand>
-        <div className="nav-item-right">
-          <Nav className="justify-content-end">
-            {!cookie.get("authed") ? (
-              <Row>
-                <Col>
-                  <Nav.Item>
-                    <a href="http://localhost:3000/login">
-                      <Button>Login</Button>
-                    </a>
-                  </Nav.Item>
-                </Col>
-                <Col className="ml-auto">
-                  <Nav.Item>
-                    <a href="http://localhost:3000/signup">
-                      <Button>Signup</Button>
-                    </a>
-                  </Nav.Item>
-                </Col>
-              </Row>
-            ) : (
-              <Row>
-                <Col>
-                  <Nav.Item>
-                    <a href="http://localhost:3000/newclass">
-                      <Button>Add Class</Button>
-                    </a>
-                  </Nav.Item>
-                </Col>
-                <Col className="ml-auto">
-                  <Nav.Item>
-                    <a href="http://localhost:3000/getclassmates">
-                      <Button>Connect</Button>
-                    </a>
-                  </Nav.Item>
-                </Col>
-              </Row>
-            )}
-          </Nav>
-        </div>
-      </Navbar>
+        <Navbar.Brand href="/">
+          CoogTree
+        </Navbar.Brand>
+        <Nav className="justify-content-end">
+            {
+              !cookie.get('authed') ? (
+                <Row>
+                  <Col><Nav.Item><a href='http://localhost:3000/login'><Button>Login</Button></a></Nav.Item></Col>
+                  <Col className="ml-auto"><Nav.Item><a href='http://localhost:3000/signup'><Button>Signup</Button></a></Nav.Item></Col>          
+                </Row>
+              ) : (
+                <Row>
+                  <Col><Nav.Item><a href='http://localhost:3000/newclass'><Button>Add Class</Button></a></Nav.Item></Col>
+                  <Col className="ml-auto"><Nav.Item><a href='http://localhost:3000/getclassmates'><Button>Connect</Button></a></Nav.Item></Col>       
+                  <Col className="ml-auto"><Nav.Item href="/"><Button onClick={handleSignout}>Signout</Button></Nav.Item></Col>
+                </Row>
+              )
+            }
+        </Nav>      
+      </Navbar> 
     </div>
   );
 };
