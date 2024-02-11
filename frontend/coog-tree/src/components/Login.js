17b7693a6f48f52d5e269d6851f13d77e51540aa
Yusuf Khan <ykhan5@cougarnet.uh.edu>
2024-02-10 21:37:02 -0600

    Added MapData Folder and JSON

---
+++ frontend/coog-tree/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginIMG from "../Assets/login-icon.png";
+
+const Login = () => {
+  return (
+    <div>
+      Login
+      <img src={LoginIMG}></img>
+    </div>
+  );
+};
+
+export default Login;
